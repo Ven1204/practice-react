b5cb9dc8f178350778ab53b2da36d69f6e53ec3f
Rayven <rayven.rey@gmail.com>
2021-12-15 22:53:18 +0100

    commits

---
+++ practice-react/index.js
@@ -0,0 +1,17 @@
+import React form "react"
+import ReactDOM from "react-dom"
+
+function MyApp() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
+
+reactDOM.render(
+  MyApp,
+  document.getElementById("root")
+)
