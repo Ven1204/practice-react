19517648d6c0f23bb929e565a3941b2cbd52be19
Rayven <rayven.rey@gmail.com>
2021-12-21 14:33:04 +0100

    added forms

--- practice-react-2/src/pages/NewMeetups.js
+++ practice-react-2/src/pages/NewMeetups.js
@@ -1,5 +1,10 @@
+import NewMeetupForm from '../components/meetups/NewMeetupFrom'
+
 function NewMeetupsPage() {
-  return <div>New Meetup</div>;
+  return <section>
+    <h1>Add New Meetup</h1>
+    <NewMeetupForm />
+  </section>;
 }
 
 export default NewMeetupsPage;
