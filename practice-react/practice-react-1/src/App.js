54dd74bf0f9b5900e96671d24e4051749f76d444
Rayven <rayven.rey@gmail.com>
2021-12-21 13:56:21 +0100

    deleted

---
+++ practice-react/practice-react-1/src/App.js
@@ -0,0 +1,12 @@
+import Todo from './components/Todo';
+
+function App() {
+  return <div>
+    <h1>My Todos</h1>
+    <Todo text="Learn React"/>
+    <Todo text="master React"/>
+    <Todo text="make project"/>
+  </div>
+}
+
+export default App;
