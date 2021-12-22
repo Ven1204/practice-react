de703acb9c7b0e9c2b04bdf105e14faae3134256
Rayven <rayven.rey@gmail.com>
2021-12-22 11:41:50 +0100

    added url for db

--- practice-react-2/src/pages/NewMeetups.js
+++ practice-react-2/src/pages/NewMeetups.js
@@ -1,10 +1,16 @@
 import NewMeetupForm from '../components/meetups/NewMeetupFrom'
 
 function NewMeetupsPage() {
-  return <section>
-    <h1>Add New Meetup</h1>
-    <NewMeetupForm />
-  </section>;
+  function addMeetupHandler(meetupData) {
+    fetch('https://react-practice-930e5-default-rtdb.firebaseio.com/meetups.json');
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
